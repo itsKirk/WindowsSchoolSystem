6769a39c879692faeb84ea1bf4e1de9d28ee03ff
Kirk <kirkrepository@gmail.com>
2022-02-09 19:27:55 +0300

    Restricted Settings Form Multiple Instance Creation

--- SchoolSystem/Screens/MainForm.cs
+++ SchoolSystem/Screens/MainForm.cs
@@ -9,60 +9,63 @@ using System.Windows.Forms;
 
 namespace SchoolSystem.Core.Screens
 {
-  public partial class MainForm : BaseForm
-  {
-    internal static (
-      List<string> EducationLevels,
-      List<string> Employers,
-      List<string> EmploymentTypes,
-      List<string> Genders,
-      List<string> Religions,
-      List<string> Titles,
-      Logo Logo) Initialization;
-    public MainForm()
+    public partial class MainForm : BaseForm
     {
-      InitializeComponent();
-      PreparePanels();
-      Initialization = InitializeThings();
-    }
-    private void PreparePanels()
-    {
-      ControlPanel.BackColor = Color.SkyBlue;
-      MainMenu.BackColor = ControlPanel.BackColor;
-      MainPanel.BackColor = Color.FloralWhite;
-      MainMenu.ForeColor = Color.WhiteSmoke;
-    }
-    private (
-      List<string>,
-      List<string>,
-      List<string>,
-      List<string>,
-      List<string>,
-      List<string>,
-      Logo) InitializeThings()
-    {
-      return
-        (
-          Factory.EducationLevels(),
-          Factory.Employers(),
-          Factory.EmploymentTypes(),
-          Factory.Genders(),
-          Factory.Religions(),
-          Factory.Titles(),
-          StartUp.GetLogo()
-        );
-    }
-    private void SettingsButton_Click(object sender, System.EventArgs e)
-    {
-      MainPanel.Controls.Add(new SettingsControl()
-      {
-        BackColor = Color.AntiqueWhite,
-        Dock = DockStyle.Fill
-      });
-    }
-    private void MainForm_Load(object sender, EventArgs e)
-    {
+        internal static (
+          List<string> EducationLevels,
+          List<string> Employers,
+          List<string> EmploymentTypes,
+          List<string> Genders,
+          List<string> Religions,
+          List<string> Titles,
+          Logo Logo) Initialization;
+        public MainForm()
+        {
+            InitializeComponent();
+            PreparePanels();
+            Initialization = InitializeThings();
+        }
+        private void PreparePanels()
+        {
+            ControlPanel.BackColor = Color.SkyBlue;
+            MainMenu.BackColor = ControlPanel.BackColor;
+            MainPanel.BackColor = Color.FloralWhite;
+            MainMenu.ForeColor = Color.WhiteSmoke;
+        }
+        private (
+          List<string>,
+          List<string>,
+          List<string>,
+          List<string>,
+          List<string>,
+          List<string>,
+          Logo) InitializeThings()
+        {
+            return
+              (
+                Factory.EducationLevels(),
+                Factory.Employers(),
+                Factory.EmploymentTypes(),
+                Factory.Genders(),
+                Factory.Religions(),
+                Factory.Titles(),
+                StartUp.GetLogo()
+              );
+        }
+        private void SettingsButton_Click(object sender, System.EventArgs e)
+        {
+            if (!(MainPanel.Controls.Count > 0))
+            {
+                MainPanel.Controls.Add(new SettingsControl()
+                {
+                    BackColor = Color.AntiqueWhite,
+                    Dock = DockStyle.Fill
+                });
+            }
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
 
+        }
     }
-  }
 }
